b304bf6afd95817508005ed996afef9cf21c6754
shaha <shahafbenjakov@gmail.com>
2020-01-23 17:00:33 +0200

    final improvements

--- the-wizard/routes/games.js
+++ the-wizard/routes/games.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 const connection = require("../bin/db/config");
 
-function getNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function getNumber(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
 
 router.get("/", function(req, res, next) {
@@ -28,11 +28,11 @@ router.get("/", function(req, res, next) {
     .catch(error => res.status(500).json({ error: error.message }));
 });
 
-router.get("/number", function(req, res, next) {
-  const {min, max} = req.body;
-  const number = getNumber(min, max);
-  res.json(number);
-});
+// router.post("/number", function(req, res, next) {
+//   const {min, max} = req.body;
+//   const number = getNumber(min, max);
+//   res.json(number);
+// });
 
 router.post("/", function(req, res, next) {
   new Promise((resolve, reject) => {
@@ -51,48 +51,40 @@ router.post("/", function(req, res, next) {
       console.log(e);
     }
   })
-    .then(values => res.status(200).json(values))
+    .then(values => res.status(200).json(values.insertId))
     .catch(error => res.status(500).json({ error: error.message }));
 });
 
 router.put("/win", function(req, res, next) {
   new Promise((resolve, reject) => {
-    try {
       const { user_number, id } = req.body;
-      query = `UPDATE games_history SET user_number = ${user_number}, number_of_guesses = number_of_guesses + 1 WHERE id = ${id}`;
-      connection.query(query, (err, result) => {
+      query = `UPDATE games_history SET user_number = ?, number_of_guesses = number_of_guesses + 1 WHERE id = ?`;
+      connection.query(query, [user_number, id], (err, result) => {
         if (err) {
           reject(err);
         } else {
           resolve(result);
         }
       });
-    } catch (e) {
-      console.log(e);
-    }
   })
     .then(values => res.status(200).json(values))
     .catch(error => res.status(500).json({ error: error.message }));
 });
 
 router.put("/guess", function(req, res, next) {
-  new Promise((resolve, reject) => {
-    try {
+  new Promise((resolve, reject) => {    
       const {computer_guesses, id } = req.body;
-      query = `UPDATE games_history SET computer_guesses = ${computer_guesses}, number_of_guesses = number_of_guesses + 1 WHERE id = ${id}`;
-      connection.query(query, (err, result) => {
+      query = `UPDATE games_history SET computer_guesses = ?, number_of_guesses = number_of_guesses + 1 WHERE id = ?`;
+      connection.query(query,[computer_guesses, id], (err, result) => {
         if (err) {
           reject(err);
         } else {
           resolve(result);
         }
       });
-    } catch (e) {
-      console.log(e);
-    }
   })
-    .then(values => res.status(200).json(values))
-    .catch(error => res.status(500).json({ error: error.message }));
+  .then(values => res.status(200).json(values))
+  .catch(error => res.status(500).json({ error: error.message }));
 });
 
 
